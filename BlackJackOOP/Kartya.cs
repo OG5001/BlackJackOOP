8e4839e76b9c4e1cf1ff4506e4b6bacdd9ee28ce
OG5001 <olahgergo917@gmail.com>
2026-02-01 12:17:09 +0100

    Kész a Kartya Class.

---
+++ BlackJackOOP/Kartya.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackOOP
+{
+    internal class Kartya
+    {
+        public enum Szin
+        {
+            PIKK,
+            TREFF,
+            KOR,
+            KARO
+        }
+
+        public enum Figura
+        {
+            KETTO = 2,
+            HAROM = 3,
+            NEGY = 4,
+            OT = 5,
+            HAT = 6,
+            HET = 7,
+            NYOLC = 8,
+            KILENC = 9,
+            TIZ = 10,
+            BUBI = 10,
+            DAMA = 10,
+            KIRALY = 10,
+            ASZ = 11
+        }
+    }
+}
+
