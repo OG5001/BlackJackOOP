3cb6ec2a2b2de201a7fc6396b040116b5c09edf0
OG5001 <olahgergo917@gmail.com>
2026-02-01 13:09:25 +0100

    Elkészült az Osztó class és a Main program is. Mostmár elvileg minden működik benne.

---
+++ BlackJackOOP/Oszto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BlackJackOOP
+{
+    internal class Oszto
+    {
+        public List<(Kartya.Szin, Kartya.Figura)> Kez { get; } = new();
+
+        public void LapotKap((Kartya.Szin, Kartya.Figura) lap)
+        {
+            Kez.Add(lap);
+        }
+
+        public int Ertek()
+        {
+            int osszeg = 0;
+            int aszok = 0;
+
+            foreach (var lap in Kez)
+            {
+                osszeg += (int)lap.Item2;
+                if (lap.Item2 == Kartya.Figura.ASZ)
+                    aszok++;
+            }
+
+            while (osszeg > 21 && aszok > 0)
+            {
+                osszeg -= 10;
+                aszok--;
+            }
+
+            return osszeg;
+        }
+
+        public void Kiir()
+        {
+            foreach (var lap in Kez)
+                Console.WriteLine($"{lap.Item1} - {lap.Item2}");
+
+            Console.WriteLine($"Érték: {Ertek()}");
+        }
+    }
+}
--- BlackJackOOP/Program.cs
+++ BlackJackOOP/Program.cs
@@ -1,10 +1,105 @@
-﻿namespace BlackJackOOP
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlackJackOOP
 {
-    internal class Program
+    namespace BlackJackOOP
     {
-        static void Main(string[] args)
+        internal class Program
         {
-            Console.WriteLine("Hello, World!");
+            static void Main(string[] args)
+            {
+                string v = "";
+                string valasz = "";
+                while (valasz != "n")
+                {
+                    Pakli pakli = new Pakli();
+                    pakli.Keszites();
+                    pakli.Keveres();
+
+                    Jatekos jatekos = new Jatekos();
+                    Jatekos oszto = new Jatekos();
+
+                    jatekos.LapotKap(pakli.Huz());
+                    jatekos.LapotKap(pakli.Huz());
+                    oszto.LapotKap(pakli.Huz());
+                    oszto.LapotKap(pakli.Huz());
+
+                    Console.WriteLine("JÁTÉKOS:");
+                    jatekos.Kiir();
+
+                    while (jatekos.Ertek() < 21)
+                    {
+                        Console.WriteLine();
+                        Console.Write("Húzol? (i/n): ");
+                        v = Console.ReadLine();
+                        Console.WriteLine();
+
+                        if (v == "i")
+                        {
+
+                            jatekos.LapotKap(pakli.Huz());
+                            jatekos.Kiir();
+                        }
+                        else if (v == "n")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Rossz választ adtál meg, próbáld újra!");
+                        }
+                    }
+
+                    if (jatekos.Ertek() > 21)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Besokalltál! Az Osztó nyert.");
+                        Console.WriteLine();
+                        continue;
+                    }
+
+                    Console.WriteLine("\nOSZTÓ:");
+                    while (oszto.Ertek() < 17)
+                    {
+                        oszto.LapotKap(pakli.Huz());
+                    }
+                    oszto.Kiir();
+
+                    if (oszto.Ertek() > 21 || jatekos.Ertek() > oszto.Ertek())
+                        Console.WriteLine("Te nyertél!");
+                    else if (jatekos.Ertek() < oszto.Ertek())
+                        Console.WriteLine("Az Osztó nyert!");
+                    else
+                        Console.WriteLine("Döntetlen!");
+                    Console.WriteLine();
+
+                    while (valasz != "i" || valasz != "n")
+                    {
+                        Console.Write("Szeretnél mégegyet játszani?(Igen: i, Nem: n): ");
+                        valasz = Console.ReadLine();
+                        if (valasz == "i")
+                        {
+                            Console.WriteLine();
+                            break;
+                        }
+                        else if (valasz == "n")
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Köszönjük hogy a Black Jacket választotta.");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Rossz választ adott meg próbáld újra!");
+                            continue;
+                        }
+                    }
+
+                }
+
+            }
         }
     }
+
 }
